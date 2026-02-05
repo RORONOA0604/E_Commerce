ab96431d60f5b13dedf7111b66c603b63dade41f
RORONOA0604 <roronovaz597@gmail.com>
2026-02-05 12:37:04 +0530

    Removed Frontend

--- Frontend/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import { UserSlice } from './slice/user.slice'
-
-export const store = configureStore({
-    reducer:{
-        [UserSlice.name]:UserSlice.reducer
-    }
-})
